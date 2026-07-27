5854750391e86aaf469a8725bc2e62bd78add636
nakilon <nakilon@gmail.com>
2026-07-27 17:22:54 +0300

    refinement inlined

--- lib/simple_llm.rb
+++ lib/simple_llm.rb
@@ -1,5 +1,18 @@
-require_relative "refinement_array"
-using ::RefinementArray
+using(
+  ::Module.new do
+    AssertOneError = ::Class.new ::RuntimeError
+    refine ::Array do
+      def assert_one msg = nil
+        return at 0 if 1 == size
+        raise ::Common::AssertOneError, "size: #{size}"
+      end
+      def assert_one_or_less
+        return at 0 if 1 >= size
+        raise ::Common::AssertOneError "size: #{size}"
+      end
+    end
+  end
+)
 
 module SimpleLLM
   require "nethttputils"
